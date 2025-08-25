3ea857ad78dfb77ad9eef8ec47512b24be41c113
diogoamss <diogoamss@gmail.com>
2025-08-25 17:47:00 -0300

    w.i.p | diogo att Animals.js

--- Index.js
+++ Index.js
@@ -1,10 +1,18 @@
 import express from 'express'
+import adocoes from './Routes/adocoes.js'
+import animal from './Routes/Animal.js'
+import usuarios from './Routes/Usuarios.js'
+import doacoes from './Routes/doacoes.js'
+import login from './Routes/login.js'
+import questionario from './Routes/Questionario.js'
+import tutores from './Routes/Tutores.js'
 
 const app = express();
 
 const port = 8080;
 
 
+app.use(express.json());
 
 app.listen(port ,() =>{
     console.log(`Servidor aberto!!\n Rodando na Porta: Localhost://${port}`);
