0df9e577ded962b1ad6c347c86ba284483b665e7
VictorDumer <victorgabrieldummersouza@gmail.com>
2025-09-01 11:26:18 -0300

    w.i.p 1.5 Models fix

--- models/Modelo.js
+++ models/Modelo.js
@@ -1,17 +1,18 @@
 import { Sequelize } from 'sequelize';
-import Animal from './Animal.js';
+import AnimalModel from './Animal.js';
 import TutorModel from './Tutor.js';
-import QuestionarioModel from './Questionario.js';
+import QuestionarioModel from './Quesitonario.js';
 import PedidoAdocaoModel from './PedidoAdocao.js';
 import DoacaoModel from './Doacao.js';
+import UsuarioModel from './Usuario.js';
 
 export const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: './Data/database.db',
 });
-
+export const Usuario = UsuarioModel(sequelize);
 export const Animal = AnimalModel(sequelize);
-export const Tutor = TutorModel(sequelize);
+export const Tutor = TutorModel(sequelize); // Tutor model quebrado
 export const Questionario = QuestionarioModel(sequelize);
 export const PedidoAdocao = PedidoAdocaoModel(sequelize);
 export const Doacao = DoacaoModel(sequelize);
@@ -25,4 +26,4 @@ export const Doacao = DoacaoModel(sequelize);
 
 await sequelize.sync();
 
-export default { sequelize, Animal, Tutor, Questionario, PedidoAdocao, Doacao };
+export default { sequelize,Usuario, Animal, Tutor, Questionario, PedidoAdocao, Doacao };
