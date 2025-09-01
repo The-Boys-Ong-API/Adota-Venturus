663e3e20a420ce59371a16bb2f58d05b47782810
Bufonn <matheus.balves6@gmail.com>
2025-09-01 17:35:06 -0300

    w.i.p 2.2 | add doacoes.js

---
+++ controllers/questionarioController.js
@@ -0,0 +1,77 @@
+/*
+
+{
+"id": "uuid",
+"nome_completo": "string",
+"rg": "string",
+"endereco": "string",
+"bairro": "string",
+"cidade": "string",
+"estado": "string",
+"celular": "string",
+"telefone": "string",
+"email": "string",
+"instagram": "string",
+"facebook": "string",
+"questionario": {
+   "empregado": true,
+   "quantos_animais_possui": 2,
+   "motivos_para_adotar": "string",
+   "quem_vai_sustentar_o_animal": "string",
+   "numero_adultos_na_casa": 2,
+   "numero_criancas_na_casa": 1,
+   "idades_criancas": \["5"\],
+   "residencia_tipo": "própria",
+   "proprietario_permite_animais": true,
+   "todos_de_acordo_com_adocao": true,
+   "responsavel_pelo_animal": "string",
+   "responsavel_concorda_com_adocao": true,
+   "ha_alergico_ou_pessoas_que_nao_gostam": false,
+   "gasto_mensal_estimado": 300,
+   "valor_disponivel_no_orcamento": true,
+   "tipo_alimentacao": "rações premium",
+   "local_que_o_animal_vai_ficar": "quintal",
+   "forma_de_permanencia": "solto 24h",
+   "forma_de_confinamento": "nenhum",
+   "tera_brinquedos": true,
+   "tera_abrigo": true,
+   "tera_passeios_acompanhado": true,
+   "tera_passeios_sozinho": false,
+   "companhia_outro_animal": true,
+   "companhia_humana_24h": true,
+   "companhia_humana_parcial": false,
+   "sem_companhia_humana": false,
+   "sem_companhia_animal": false,
+   "o_que_faz_em_viagem": "deixa com parente",
+   "o_que_faz_se_fugir": "procura imediatamente",
+   "o_que_faz_se_nao_puder_criar": "procura nova família",
+   "animais_que_ja_criou": "2 cães, 1 gato",
+   "destino_animais_anteriores": "todos faleceram de velhice",
+   "costuma_esterilizar": true,
+   "costuma_vacinar": true,
+   "costuma_vermifugar": true,
+   "veterinario_usual": "Clínica PetVida",
+   "forma_de_educar": "reforço positivo",
+   "envia_fotos_e_videos_do_local": true,
+   "aceita_visitas_e_fotos_do_animal": true,
+   "topa_entrar_grupo_adotantes": true,
+   "concorda_com_taxa_adocao": true,
+   "data_disponivel_para_buscar_animal": "2024-12-20"
+   }
+}
+
+*/
+
+import { Questionario as questionario } from "../models/Modelo";
+
+const questionarioController = {
+
+    async cadastroQuestionario (req, res){
+
+    }
+
+
+
+}
+
+export default questionarioController;
