7a44612e6fd0c94a083c8ef0dd96e5deba55e76c
VictorDumer <victorgabrieldummersouza@gmail.com>
2025-09-08 16:27:34 -0300

    w.i.p explanation

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import usuarioRouter from './Routes/usuarioRouter.js';
-
+// import animaisRouter
 const app = express();
 
 app.use(express.json());
-
+// app.use('/animais', animaisRouters);
 app.use('/tutores', usuarioRouter);
 
 app.listen(3000, () => {
